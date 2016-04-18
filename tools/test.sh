59cf372d871c46cf3972a806e8ee4c4438a9d60d
xander <xander@gerrmann.nl>
2016-04-18 09:52:45 +0200

    Fix testing for one model

--- tools/test.sh
+++ tools/test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # Must be run from dir 'py-faster-rcnn', via: './tools/test.sh'
-./tools/test_net.py --gpu 0 --def models/apc/VGG16/faster_rcnn_end2end/test.prototxt --model_path /home/$USER/py-faster-rcnn/output/faster_rcnn_end2end/apc_2016_trainval/vgg16_faster_rcnn_iter_5000.caffemodel --imdb apc_test_validated --cfg experiments/cfgs/faster_rcnn_end2end.yml --vis
+#./tools/test_net.py --gpu 0 --def models/apc/VGG16/faster_rcnn_end2end/test.prototxt --model_path /home/$USER/py-faster-rcnn/output/faster_rcnn_end2end/apc_2016_trainval/ --imdb apc_test_validated --cfg experiments/cfgs/faster_rcnn_end2end.yml --vis --eval_iters True
+./tools/test_net.py --gpu 0 --def models/apc/VGG16/faster_rcnn_end2end/test.prototxt --net /home/$USER/py-faster-rcnn/output/faster_rcnn_end2end/apc_old_models/vgg16_faster_rcnn_iter_10000.caffemodel --imdb apc_test_validated --cfg experiments/cfgs/faster_rcnn_end2end.yml --thresh 0.05
+#./tools/test_net.py --gpu 0 --def models/apc/VGG16/faster_rcnn_end2end/test.prototxt --model_path /home/$USER/py-faster-rcnn/output/faster_rcnn_end2end/vgg16_faster_rcnn_iter_10000.caffemodel --imdb apc_test_validated --cfg experiments/cfgs/faster_rcnn_end2end.yml --eval_iters True --thresh 0.05
--- tools/test_net.py
+++ tools/test_net.py
@@ -20,51 +20,51 @@ import pprint
 import time, os, sys
 
 def parse_args():
-    """
-    Parse input arguments
-    """
-    parser = argparse.ArgumentParser(description='Test a Fast R-CNN network')
-    parser.add_argument('--gpu', dest='gpu_id', help='GPU id to use',
-                        default=0, type=int)
-    parser.add_argument('--def', dest='prototxt',
-                        help='prototxt file defining the network',
-                        default=None, type=str)
-    parser.add_argument('--model_path', dest='caffe_model_path',
-                        help='path to caffe model or path to directory if --eval_iters = True',
-                        default=None, type=str)
-    parser.add_argument('--cfg', dest='cfg_file',
-                        help='optional config file', default=None, type=str)
-    parser.add_argument('--wait', dest='wait',
-                        help='wait until net file exists',
-                        default=True, type=bool)
-    parser.add_argument('--imdb', dest='imdb_name',
-                        help='dataset to test',
-                        default='apc_test_validated', type=str)
-    parser.add_argument('--comp', dest='comp_mode', help='competition mode',
-                        action='store_true')
-    parser.add_argument('--set', dest='set_cfgs',
-                        help='set config keys', default=None,
-                        nargs=argparse.REMAINDER)
-    parser.add_argument('--vis', dest='vis', help='visualize detections',
-                        action='store_true')
-    parser.add_argument('--num_dets', dest='max_per_image',
-                        help='max number of detections per image',
-                        default=100, type=int)
-    parser.add_argument('--eval_iters', dest='eval_iters',
+	"""
+	Parse input arguments
+	"""
+	parser = argparse.ArgumentParser(description='Test a Fast R-CNN network')
+	parser.add_argument('--gpu', dest='gpu_id', help='GPU id to use',
+						default=0, type=int)
+	parser.add_argument('--def', dest='prototxt',
+						help='prototxt file defining the network',
+						default=None, type=str)
+	parser.add_argument('--net', dest='caffe_model_path',
+						help='path to caffe model or path to directory if --eval_iters = True',
+						default=None, type=str)
+	parser.add_argument('--cfg', dest='cfg_file',
+						help='optional config file', default=None, type=str)
+	parser.add_argument('--wait', dest='wait',
+						help='wait until net file exists',
+						default=True, type=bool)
+	parser.add_argument('--imdb', dest='imdb_name',
+						help='dataset to test',
+						default='apc_test_validated', type=str)
+	parser.add_argument('--comp', dest='comp_mode', help='competition mode',
+						action='store_true')
+	parser.add_argument('--set', dest='set_cfgs',
+						help='set config keys', default=None,
+						nargs=argparse.REMAINDER)
+	parser.add_argument('--vis', dest='vis', help='visualize detections',
+						action='store_true')
+	parser.add_argument('--num_dets', dest='max_per_image',
+						help='max number of detections per image',
+						default=100, type=int)
+	parser.add_argument('--eval_iters', dest='eval_iters',
 						help='test multiple models to evaluate the effect of iterations',
 						default=False, type=bool)
 	parser.add_argument('--thresh',dest='thresh_detect',
 						help='Threshold for detection',
-						type=float,default=0.3)
+						type=float,
+						default=0.3)
 
-    if len(sys.argv) == 1:
-        parser.print_help()
-        print "No arguments given, exit"
-        sys.exit(1)
+	if len(sys.argv) == 1:
+		parser.print_help()
+		print "No arguments given, exit"
+		sys.exit(1)
 
-    args = parser.parse_args()
-    return args
-
+	args = parser.parse_args()
+	return args
 
 
 def get_iterations_from_filename(filename):
@@ -122,7 +122,7 @@ if __name__ == '__main__':
 	performance_list	= []
 	iterations_list		= []
 	
-	print sorted(os.listdir(args.caffe_model_path))
+	#print sorted(os.listdir(args.caffe_model_path))
 	
 	for model_file in model_paths:
 		print "caffemodel:"
@@ -137,7 +137,7 @@ if __name__ == '__main__':
 		model_path = os.path.join(args.caffe_model_path,model_file)
 		net = caffe.Net(args.prototxt, model_path, caffe.TEST)
 		net.name = os.path.splitext(os.path.basename(model_file))[0]
-		(classes, performance) = test_net(net, imdb, max_per_image=args.max_per_image, thresh=thresh_detect, vis=args.vis)
+		(classes, performance) = test_net(net, imdb, max_per_image=args.max_per_image, thresh=args.thresh_detect, vis=args.vis)
 		performance_list += [performance]
 	
 	if(args.eval_iters == True):
